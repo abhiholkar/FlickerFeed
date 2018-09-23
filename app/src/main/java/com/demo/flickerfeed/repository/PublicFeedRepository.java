86bbb062ba47e6643b0318bf95f3e3e86d1359b2
Abhi Holkar <holkar.abhinav@gmail.com>
2018-09-23 20:12:00 +0100

    Set feed repeat in 30 secs.

--- app/src/main/java/com/demo/flickerfeed/repository/PublicFeedRepository.java
+++ app/src/main/java/com/demo/flickerfeed/repository/PublicFeedRepository.java
@@ -3,10 +3,14 @@ package com.demo.flickerfeed.repository;
 import com.demo.flickerfeed.model.FlickerFeedResponse;
 import com.demo.flickerfeed.network.APIService;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Inject;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 
@@ -25,6 +29,12 @@ public class PublicFeedRepository {
 
     public Observable<FlickerFeedResponse> getPublicFeeds() {
         return retrofit.create(APIService.class).getFlickerPublicFeeds()
+                .repeatWhen(new Function<Observable<Object>, ObservableSource<?>>() {
+                    @Override
+                    public ObservableSource<?> apply(Observable<Object> objectObservable) throws Exception {
+                        return objectObservable.delay(30, TimeUnit.SECONDS);
+                    }
+                })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
